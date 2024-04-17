9844721f31860beb33247a304fa894c13fbd1b09
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-04-17 23:08:57 +0500

    feat: compose

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 if not os.getenv("PROD"):
-    load_dotenv('.env')
+    load_dotenv('.env.example')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
